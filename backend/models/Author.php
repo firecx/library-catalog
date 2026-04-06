e061fb1d3b5cea255fc4b364fcfe87fddcee4430
Firecx <nikandrov.alekseix1@gmail.com>
2026-04-06 20:24:09 +0600

    add Author model

---
+++ backend/models/Author.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Models;
+
+use Config\Database;
+use PDO;
+
+class Author {
+    private PDO $db;
+
+    public function __construct() {
+        $this->db = Database::connect();
+    }
+
+    public function getAll(): array {
+        $stmt = $this->db->query('SELECT * FROM authors ORDER BY id');
+        return $stmt->fetchAll();
+    }
+
+    public function getById(int $id): ?array {
+        $stmt = $this->db->prepare('SELECT * FROM authors WHERE id = :id');
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch() ?: null;
+    }
+
+    public function create(string $name): bool {
+        $stmt = $this->db->prepare("INSERT INTO authors (name) VALUES (:name)");
+        return $stmt->execute(['name' => $name]);
+    }
+
+    public function delete(int $id): bool {
+        $stmt = $this->db->prepare("DELETE FROM authors WHERE id = :id");
+        return $stmt->execute(['id' => $id]);
+    }
+}
