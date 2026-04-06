cefc27e7944e5f1bc43971177818280678459b11
Firecx <nikandrov.alekseix1@gmail.com>
2026-04-06 20:42:26 +0600

    fix

--- backend/models/Author.php
+++ backend/models/Author.php
@@ -13,25 +13,28 @@ class Author {
     }
 
     public function getAll(): array {
-        $stmt = $this->db->query('SELECT * FROM authors ORDER BY id');
+        $sql = "SELECT * FROM authors ORDER BY id";
+        $stmt = $this->db->query($sql);
         return $stmt->fetchAll();
     }
 
     public function getById(int $id): ?array {
-        $stmt = $this->db->prepare('SELECT * FROM authors WHERE id = :id');
+        $sql = "SELECT * FROM authors WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
         $stmt->execute(['id' => $id]);
         return $stmt->fetch() ?: null;
     }
 
     public function create(string $name): bool {
-        $stmt = $this->db->prepare("INSERT INTO authors (name)
-                                    VALUES (:name)");
+        $sql = "INSERT INTO authors (name) VALUES (:name)";
+        $stmt = $this->db->prepare($sql);
         $stmt->execute(['name' => $name]);
         return $stmt->fetch();
     }
 
     public function delete(int $id): bool {
-        $stmt = $this->db->prepare("DELETE FROM authors WHERE id = :id");
+        $sql = "DELETE FROM authors WHERE id = :id";
+        $stmt = $this->db->prepare($sql);
         return $stmt->execute(['id' => $id]);
     }
 }
